3ad4bea1aa0bac66094acb9be1aee8c2f944a7ec
Vladimirs Anufrijevs <vladimirs.anufrijevs@gmail.com>
2025-01-22 20:56:08 +0200

    add additional getters, setters & override equals & hashCode

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Employee {
 
     private static int idGenerator = 1;
@@ -31,6 +33,14 @@ public class Employee {
         return id;
     }
 
+    public int getIdGenerator() {
+        return idGenerator;
+    }
+
+    public void setIdGenerator(int idGenerator) {
+        Employee.idGenerator = idGenerator;
+    }
+
     public void setDepartment(int department) {
         this.department = department;
     }
@@ -46,20 +56,16 @@ public class Employee {
                 ", department=" + department +
                 ", salary=" + salary;
     }
-}
 
-//Создать класс Employee, который содержит информацию о ФИО, отделе и зарплате сотрудника. Отделы для простоты должны быть названы от 1 до 5.
-//Добавить статическую переменную-счетчик, которая будет отвечать за id.
-//Добавить в класс Employee поле id, которое проставляется из счетчика, а затем счетчик увеличивает свое значение.
-//Скрыть прямой доступ к полям класса Employee и добавить возможность получать значения полей
-//        (геттеры) и устанавливать значения полей отдела и зарплаты (сеттеры).
-//Реализовать в классе Employee контракты equals и hashCode.
-//По-умолчанию все поля должны передаваться через конструктор (кроме id) и заполняться в нем (включая id, который нужно получить из счетчика).
-//Создать внутри класса Main метод main и поле типа Employee[10], которое будет выполнять роль "хранилища" для записей о сотрудниках.
-//Создать статические методы, которые будут взаимодействовать с массивом из пункта 6 и возвращать результат:
-//Получить список всех сотрудников со всеми имеющимися по ним данными (вывести в консоль значения всех полей (toString));
-//Посчитать сумму затрат на ЗП в месяц;
-//Найти сотрудника с минимальной ЗП;
-//Найти сотрудника с максимальной ЗП;
-//Подсчитать среднее значение зарплат (можно использовать для этого метод из пункта b);
-//Распечатать ФИО всех сотрудников (метод ничего).
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && department == employee.department && salary == employee.salary && Objects.equals(fullName, employee.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id, fullName, department, salary);
+    }
+}
